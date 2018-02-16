128af75e1c8673642d1ae5fa078d12ba9b2e3ceb
Szabina Hazi <szabina.hazi@flowacademy.hu>
2018-02-16 09:18:18 +0100

    spagetti deleted from collision

--- collision-check.js
+++ collision-check.js
@@ -1,75 +1,21 @@
 function checkCollision(board, ship) {
   let isCollision = false;
+  let i;
+  let j;
   if (ship.vertOrHor === 1) { // vertical
-    if (ship.shipSize === 5) {
-      while (board[ship.startx - 1][ship.starty] || board[ship.startx][ship.starty] || board[ship.startx + 1][ship.starty] || board[ship.startx + 2][ship.starty] || board[ship.startx + 3][ship.starty] || board[ship.startx + 4][ship.starty] || board[ship.startx + 5][ship.starty] ||
-        board[ship.startx - 1][ship.starty - 1] || board[ship.startx][ship.starty - 1] || board[ship.startx + 1][ship.starty - 1] || board[ship.startx + 2][ship.starty - 1] || board[ship.startx + 3][ship.starty - 1] || board[ship.startx + 4][ship.starty - 1] || board[ship.startx + 5][ship.starty - 1] ||
-        board[ship.startx - 1][ship.starty + 1] || board[ship.startx][ship.starty + 1] || board[ship.startx + 1][ship.starty + 1] || board[ship.startx + 2][ship.starty + 1] || board[ship.startx + 3][ship.starty + 1] || board[ship.startx + 4][ship.starty + 1] || board[ship.startx + 5][ship.starty + 1] !== 0) {
-        isCollision = true;
-      }
-    }
-    if (ship.shipSize === 4) {
-      while (board[ship.startx - 1][ship.starty] || board[ship.startx][ship.starty] || board[ship.startx + 1][ship.starty] || board[ship.startx + 2][ship.starty] || board[ship.startx + 3][ship.starty] || board[ship.startx + 4][ship.starty] ||
-      board[ship.startx - 1][ship.starty - 1] || board[ship.startx][ship.starty - 1] || board[ship.startx + 1][ship.starty - 1] || board[ship.startx + 2][ship.starty - 1] || board[ship.startx + 3][ship.starty - 1] || board[ship.startx + 4][ship.starty - 1] ||
-      board[ship.startx - 1][ship.starty + 1] || board[ship.startx][ship.starty + 1] || board[ship.startx + 1][ship.starty + 1] || board[ship.startx + 2][ship.starty + 1] || board[ship.startx + 3][ship.starty + 1] || board[ship.startx + 4][ship.starty + 1] !== 0) {
-        isCollision = true;
-      }
-    }
-    if (ship.shipSize === 3) {
-      while (board[ship.startx - 1][ship.starty] || board[ship.startx][ship.starty] || board[ship.startx + 1][ship.starty] || board[ship.startx + 2][ship.starty] || board[ship.startx + 3][ship.starty] ||
-        board[ship.startx - 1][ship.starty - 1] || board[ship.startx][ship.starty - 1] || board[ship.startx + 1][ship.starty - 1] || board[ship.startx + 2][ship.starty - 1] || board[ship.startx + 3][ship.starty - 1] ||
-        board[ship.startx - 1][ship.starty + 1] || board[ship.startx][ship.starty + 1] || board[ship.startx + 1][ship.starty + 1] || board[ship.startx + 2][ship.starty + 1] || board[ship.startx + 3][ship.starty + 1] !== 0) {
-        isCollision = true;
-      }
-    }
-    if (ship.shipSize === 2) {
-      while (board[ship.startx - 1][ship.starty] || board[ship.startx][ship.starty] || board[ship.startx + 1][ship.starty] || board[ship.startx + 2][ship.starty] ||
-        board[ship.startx - 1][ship.starty - 1] || board[ship.startx][ship.starty - 1] || board[ship.startx + 1][ship.starty - 1] || board[ship.startx + 2][ship.starty - 1] ||
-        board[ship.startx - 1][ship.starty + 1] || board[ship.startx][ship.starty + 1] || board[ship.startx + 1][ship.starty + 1] || board[ship.startx + 2][ship.starty + 1] !== 0) {
-        isCollision = true;
-      }
-    }
-    if (ship.shipSize === 1) {
-      while (board[ship.startx - 1][ship.starty] || board[ship.startx][ship.starty] || board[ship.startx + 1][ship.starty] ||
-        board[ship.startx - 1][ship.starty - 1] || board[ship.startx][ship.starty - 1] || board[ship.startx + 1][ship.starty - 1] ||
-        board[ship.startx - 1][ship.starty + 1] || board[ship.startx][ship.starty + 1] || board[ship.startx + 1][ship.starty + 1] !== 0) {
-        isCollision = true;
+    for (i = ship.startx - 1; i < ship.shipSize + 1; i++) {
+      for (j = ship.starty - 1; j < ship.starty + 1; j++) {
+        while (board[i][j] === 0) {
+          isCollision = true;
+        }
       }
     }
   } else { // horizontal
-    if (ship.shipSize === 5) {
-      while (board[ship.startx][ship.starty - 1] || board[ship.startx][ship.starty] || board[ship.startx][ship.starty + 1] || board[ship.startx][ship.starty + 2] || board[ship.startx][ship.starty + 3] || board[ship.startx][ship.starty + 4] || board[ship.startx][ship.starty + 5] ||
-        board[ship.startx - 1][ship.starty - 1] || board[ship.startx - 1][ship.starty] || board[ship.startx - 1][ship.starty + 1] || board[ship.startx - 1][ship.starty + 2] || board[ship.startx - 1][ship.starty + 3] || board[ship.startx - 1][ship.starty + 4] || board[ship.startx - 1][ship.starty + 5] ||
-        board[ship.startx + 1][ship.starty - 1] || board[ship.startx + 1][ship.starty] || board[ship.startx + 1][ship.starty + 1] || board[ship.startx + 1][ship.starty + 2] || board[ship.startx + 1][ship.starty + 3] || board[ship.startx + 1][ship.starty + 4] || board[ship.startx + 1][ship.starty + 5] !== 0) {
-        isCollision = true;
-      }
-    }
-    if (ship.shipSize === 4) {
-      while (board[ship.startx][ship.starty - 1] || board[ship.startx][ship.starty] || board[ship.startx][ship.starty + 1] || board[ship.startx][ship.starty + 2] || board[ship.startx][ship.starty + 3] || board[ship.startx][ship.starty + 4] ||
-        board[ship.startx - 1][ship.starty - 1] || board[ship.startx - 1][ship.starty] || board[ship.startx - 1][ship.starty + 1] || board[ship.startx - 1][ship.starty + 2] || board[ship.startx - 1][ship.starty + 3] || board[ship.startx - 1][ship.starty + 4] ||
-        board[ship.startx + 1][ship.starty - 1] || board[ship.startx + 1][ship.starty] || board[ship.startx + 1][ship.starty + 1] || board[ship.startx + 1][ship.starty + 2] || board[ship.startx + 1][ship.starty + 3] || board[ship.startx + 1][ship.starty + 4] !== 0) {
-        isCollision = true;
-      }
-    }
-    if (ship.shipSize === 3) {
-      while (board[ship.startx][ship.starty - 1] || board[ship.startx][ship.starty] || board[ship.startx][ship.starty + 1] || board[ship.startx][ship.starty + 2] || board[ship.startx][ship.starty + 3] ||
-        board[ship.startx - 1][ship.starty - 1] || board[ship.startx - 1][ship.starty] || board[ship.startx - 1][ship.starty + 1] || board[ship.startx - 1][ship.starty + 2] || board[ship.startx - 1][ship.starty + 3] ||
-        board[ship.startx + 1][ship.starty - 1] || board[ship.startx + 1][ship.starty] || board[ship.startx + 1][ship.starty + 1] || board[ship.startx + 1][ship.starty + 2] || board[ship.startx + 1][ship.starty + 3] !== 0) {
-        isCollision = true;
-      }
-    }
-    if (ship.shipSize === 2) {
-      while (board[ship.startx][ship.starty - 1] || board[ship.startx][ship.starty] || board[ship.startx][ship.starty + 1] || board[ship.startx][ship.starty + 2] ||
-        board[ship.startx - 1][ship.starty - 1] || board[ship.startx - 1][ship.starty] || board[ship.startx - 1][ship.starty + 1] || board[ship.startx - 1][ship.starty + 2] ||
-        board[ship.startx + 1][ship.starty - 1] || board[ship.startx + 1][ship.starty] || board[ship.startx + 1][ship.starty + 1] || board[ship.startx + 1][ship.starty + 2] !== 0) {
-        isCollision = true;
-      }
-    }
-    if (ship.shipSize === 1) {
-      while (board[ship.startx][ship.starty - 1] || board[ship.startx][ship.starty] || board[ship.startx][ship.starty + 1] ||
-        board[ship.startx - 1][ship.starty - 1] || board[ship.startx - 1][ship.starty] || board[ship.startx - 1][ship.starty + 1] ||
-        board[ship.startx + 1][ship.starty - 1] || board[ship.startx + 1][ship.starty] || board[ship.startx + 1][ship.starty + 1] !== 0) {
-        isCollision = true;
+    for (j = ship.starty - 1; j < ship.shipSize + 1; j++) {
+      for (i = ship.startx - 1; i < ship.startx + 1; i++) {
+        while (board[i][j] === 0) {
+          isCollision = true;
+        }
       }
     }
   }
