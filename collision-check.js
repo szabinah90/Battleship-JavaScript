ae25c9352ec80b32c05e247882b3fc46dfcc2f3d
Szabina Hazi <szabina.hazi@flowacademy.hu>
2018-02-16 14:55:24 +0100

    generation done (generate ships ok; collision ok, put ships ok)

--- collision-check.js
+++ collision-check.js
@@ -2,30 +2,27 @@ function checkCollision(board, ship) {
   let i;
   let j;
   let collision = false;
-  let checkSumCells = 0;
+  let checkSumIJ = 0;
   let checkSumI = 0;
   let checkSumJ = 0;
   if (ship.vertOrHor === 1) { // vertical
-    for (i = ship.startx - 1; i < ship.startx + ship.shipSize + 1; i++) {
-      checkSumI = +i;
-      for (j = ship.starty - 1; j < ship.starty + 2; j++) {
-        checkSumJ = +j;
+    for (i = (ship.startx - 1); i < (ship.startx + ship.shipSize + 1); i++) {
+      for (j = (ship.starty - 1); j < (ship.starty + 2); j++) {
+        checkSumIJ += board[i][j];
       }
-      checkSumCells = checkSumJ + checkSumI;
     }
   } else { // horizontal
-    for (j = ship.starty - 1; j < ship.starty + ship.shipSize + 1; j++) {
-      checkSumJ = +j;
-      for (i = ship.startx - 1; i < ship.startx + 2; i++) {
-        checkSumI = +i;
+    for (j = (ship.starty - 1); j < (ship.starty + ship.shipSize + 1); j++) {
+
+      for (i = (ship.startx - 1); i < (ship.startx + 2); i++) {
+        checkSumIJ += board[i][j];
       }
-      checkSumCells = checkSumJ + checkSumI;
     }
   }
-  if (checkSumCells === (ship.shipSize * ship.shipSize)) {
-    collision = true;
-  } else {
+  if (checkSumIJ == 0) {
     collision = false;
+  } else {
+    collision = true;
   }
   return collision;
 }
--- getShips.js
+++ getShips.js
@@ -3,26 +3,18 @@ const generateShips = require('./generateShips.js');
 const checkCollision = require('./collision-check.js');
 const putShipsOnBoard = require('./put-ships-on-board.js');
 
-/*let arrayBoard = battleshipMatrix.arrayGenerator(13, 13);
-arrayBoard[3][2] = 3;
-arrayBoard[3][3] = 3;
-arrayBoard[3][4] = 3;
-*/
-
-// console.log(battleshipMatrix.displayMatrix(arrayBoard));
-
 let board = battleshipMatrix.arrayGenerator(12, 12);
 
 function getShips(boardsize, board) {
   let i;
   for (i = 5; i > 0; i--) {
     let newShips = [];
-    let ship = generateShips.generateShips(boardsize, i);
+    let ship = generateShips.generateShips(boardsize, i); // @todo: rename generateShip
     let collision = checkCollision.checkCollision(board, ship);
     if (collision === false) {
       board = putShipsOnBoard.putShipsOnBoard(board, ship);
     } else {
-      i = 6;
+      i = i + 1;
     }
   }
   return board;
