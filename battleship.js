8eca09a652611c319fef77425c52b8a3d4daeec1
Szabina Hazi <szabina.hazi@flowacademy.hu>
2018-02-20 10:44:29 +0100

    battleship.js switch + artwork

--- battleship.js
+++ battleship.js
@@ -6,6 +6,7 @@ const guessing = require('./guessing.js');
 const putShipsOnBoard = require('./put-ships-on-board.js');
 const userInputInterpreter = require('./user-input-interpreter.js');
 const readlineSync = require('readline-sync');
+const artWork = require('./battleshipArt.js');
 
 function generatePlayground() {
   let playerBoard = battleshipMatrix.displayMatrix(battleshipMatrix.arrayGenerator(12, 12, 0));
@@ -20,16 +21,15 @@ function gameLoop() {
   let playBoard = generatedOutput[1];
   let playShips = generatedOutput[0];
   let playBoardSeen = battleshipMatrix.displayMatrix(battleshipMatrix.arrayGenerator(12, 12, ' '));
+  console.log('\33c');
+  artWork.artWork();
   console.log(playBoardSeen);
   let counter = 0;
 
   while (counter !== 5) {
-    let uiCol = readlineSync.keyIn('Please enter a column (capital letters: A-J): ', { limit: '$<ABCDEFGHIJ>' });
+    let uiCol = readlineSync.keyIn('Please enter a column (capital letters: A-J): ', { limit: '$<A-J>, caseSensitive: true' });
     uiCol = userInputInterpreter.userInputInterpreter(uiCol);
     let uiRow = readlineSync.question('Please enter a row (numbers: 1-10): ');
-    if (uiRow < 1 || uiRow > 10) {
-      console.log('Invalid row. Please try again.');
-    }
 
     let guess = guessing.guessing(playBoard, playBoardSeen, playShips, uiRow, uiCol);
     console.log('\33c');
@@ -45,6 +45,17 @@ function gameLoop() {
     }
   }
   console.log('Congratulation! You sank all the battleships!')
+  let newGame = readlineSync.keyIn('Would you like to play again? (y/n)', { limit: '$<nNyY>' });
+  switch (newGame) {
+    case ('Y' || 'y'):
+    console.log('\33c');
+    gameLoop();
+    break;
+    case ('N' || 'n'):
+    console.log('Thank you for playing our game! We hope we will meet again!');
+    process.exit;
+    break;
+  }
 }
 
 gameLoop();
---
+++ battleshipArt.js
@@ -0,0 +1,21 @@
+function artWork () {
+  console.log('                                     |__');
+  console.log('                                     |\\/');
+  console.log('                                     ---');
+  console.log('                                     / | [');
+  console.log('                              !      | |||');
+  console.log('                            _/|     _/|-++\'');
+  console.log('                        +  +--|    |--|--|_ |-');
+  console.log('                     { /|__|  |/\\__|  |--- |||__/');
+  console.log('                    +---------------___[}-_===_.\'____                 /\\');
+  console.log('                ____`-\' ||___-{]_| _[}-  |     |_[___\\==--            \\/   _');
+  console.log(' __..._____--==/___]_|__|_____________________________[___\\==--____,------\' .7');
+  console.log('|                                                                     BB-61/');
+  console.log(' \\_________________________________________________________________________|');
+  console.log('\n');
+  console.log('\n');
+}
+
+module.exports = {
+  artWork: artWork
+};
