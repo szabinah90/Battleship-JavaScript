75a313de6b4b99c99888ca60e355f76bf2e71425
Szabina Hazi <szabina.hazi@flowacademy.hu>
2018-02-21 12:36:06 +0100

    counter included

--- battleship.js
+++ battleship.js
@@ -17,12 +17,12 @@ function gameLoop() {
   let playBoardSeen = battleshipMatrix.displayMatrix(battleshipMatrix.arrayGenerator(12, 12, '?'));
   console.log('\33c');
   artWork.artWork();
-  
-  battleshipMatrix.print(battleshipMatrix.genUserBoard(playBoardSeen));
-  
-  let counter = 0;
 
-  while (counter !== 5) {
+  battleshipMatrix.print(battleshipMatrix.genUserBoard(playBoardSeen));
+
+  var counter = 0;
+  var torpedoCounter = 100;
+
     let uiCol = readlineSync.keyIn('\nPlease enter a column (capital letters: A-J): ', { limit: '$<A-J><!>', caseSensitive: true });
     if (uiCol === '!') {
       console.log('\nThank you for playing our game! Try to finish it next time!');
@@ -35,25 +35,43 @@ function gameLoop() {
     console.log('\33c');
     artWork.artWork();
     battleshipMatrix.print(battleshipMatrix.genUserBoard(playBoardSeen));
-    
+
     console.log('\n' + guess);
 
     if (guess === 'You sank a battleship!') {
       counter += 1;
     }
+    console.log(counter);
+    torpedoCounter--
+    console.log('Torpedoes left: ' + torpedoCounter);
+    if (torpedoCounter === 0) {
+      console.log('\nSorry, you ran out of torpedos. You lost.')
+      let newGame = readlineSync.keyIn('Would you like to play again? (y/n)', { limit: '$<nNyY>' });
+      switch (newGame) {
+        case ('Y' || 'y'):
+          console.log('\33c');
+          gameLoop();
+          break;
+        case ('N' || 'n'):
+          console.log('\nThank you for playing our game! We hope we will meet again!');
+          process.exit();
+          break;
+      }
+    }
+    if (counter === 5) {
+      console.log('\nCongratulations! You sank all the battleships!')
+      newGame = readlineSync.keyIn('Would you like to play again? (y/n)', { limit: '$<nNyY>' });
+      switch (newGame) {
+        case ('Y' || 'y'):
+          console.log('\33c');
+          gameLoop();
+          break;
+        case ('N' || 'n'):
+          console.log('\nThank you for playing our game! We hope we will meet again!');
+          process.exit();
+          break;
+      }
+    }
   }
-  console.log('\nCongratulations! You sank all the battleships!')
-  let newGame = readlineSync.keyIn('Would you like to play again? (y/n)', { limit: '$<nNyY>' });
-  switch (newGame) {
-    case ('Y' || 'y'):
-      console.log('\33c');
-      gameLoop();
-      break;
-    case ('N' || 'n'):
-      console.log('\nThank you for playing our game! We hope we will meet again!');
-      process.exit();
-      break;
-  }
-}
 
 gameLoop();
